3f5b75b129b34583d3a526ac6253397bd28bd0f9
toopeachok <2peachok@gmail.com>
2021-07-16 08:37:22 +0300

    Get extended clients data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 transactions_data = pd.read_csv('transactions.csv', sep=';')
+clients_data = pd.read_csv('clients.csv', sep=',')
 
 transactions_data['tran_dttm'] = pd.to_datetime(transactions_data['tran_dttm'])
 transactions_data['merchant_category'] = transactions_data['merchant_category'].replace(
@@ -18,11 +19,26 @@ transactions_data = transactions_data.drop(
     ['Продукты и товары повседневного спроса', 'brand', 'credit_tran_amt', 'deposit_tran_amt', 'is_nfc_pay',
      'is_action',
      'term', 'brand_rk'], axis=1)
-one_hot = one_hot.drop('Продукты и товары повседневного спроса', axis=1)
+one_hot = one_hot.drop(['Продукты и товары повседневного спроса'], axis=1)
 
 clients_groups = transactions_data.groupby('client_id')
-gas_stations_info = transactions_data[transactions_data['Заправки'] == 1].groupby('client_id')
-print(gas_stations_info['tran_amt'].apply(sum))
-print(clients_groups['Заправки'].apply(sum))
-print(clients_groups.tran_amt.sum())
-print(transactions_data)
+clients_data = clients_data[clients_data['client_id'].isin(clients_groups.groups)]
+
+purchases_frequency = {}
+purchases_amount = {}
+# for col_name in ['Заправки']:
+for col_name in one_hot.columns:
+    pfs = clients_groups[col_name].apply(sum)
+    purchases_frequency[col_name] = pfs
+
+    pas = transactions_data[transactions_data[col_name] == 1].groupby('client_id')['tran_amt'].apply(sum)
+    purchases_amount[col_name] = pas
+
+# for col_name in ['Заправки']:
+for col_name in one_hot.columns:
+    temp_df = pd.concat([purchases_frequency[col_name], purchases_amount[col_name]], axis=1)
+    temp_df.columns = [col_name + ' frequency', col_name + ' amount']
+    temp_df = temp_df.fillna(0)
+    clients_data = pd.merge(clients_data, temp_df, on='client_id')
+
+clients_data.to_csv('extended_clients_data.csv')
